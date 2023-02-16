516162e4b40f62d2b0e81d769c3679e0b65356e2
Oleksiy Hrybach <a.a.gribach@gmail.com>
2023-02-16 19:26:20 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let regularNumber = n < 0 ? n * -1 : n;
+    let revNum = 0;
+    let lastDigit;
+
+    while (regularNumber !== 0) {
+        lastDigit = regularNumber % 10;
+        revNum = revNum * 10 + lastDigit;
+        regularNumber = ~~(regularNumber / 10);
+    }
+
+    return revNum;
+};
